01e45767e16f5dbb6096688e8a6680838458b2eb
Yannick Formaggio <yannick.formaggio@gmail.com>
2016-03-14 18:28:35 -0700

    Added usbredir 64bits support

--- usbscapy.py
+++ usbscapy.py
@@ -87,11 +87,18 @@ usbredir_caps_enum = {
 
 
 # DO NOT FUZZ THE FOLLOWING REDIR SPECIFIC PACKAGES! FUZZING WILL CAUSE IN QEMU CRASH!
-class usbredirheader(Packet):
+class usbredirheader_32(Packet):
     name = "UsbredirPacket"
     fields_desc = [LEIntEnumField("Htype", -1, usbredir_type_enum),
                    LEIntField("HLength", 0),
                    LEIntField("Hid", -1)]
+
+
+class usbredirheader(Packet):
+    name = "UsbredirPacket"
+    fields_desc = [LEIntEnumField("Htype", -1, usbredir_type_enum),
+                   LEIntField("HLength", 0),
+                   XLongField("Hid", -1)]
 
 # Redir Packet No. 0 (redir hello)
 class hello_redir_header(Packet):
